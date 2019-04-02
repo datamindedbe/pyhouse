7dc05b26718cc07a89bc75c36c35bee3ad87d0c6
Pedro Santos <pedro.santos@dataminded.be>
2019-04-02 14:37:20 +0200

    new spark session

--- lighthouse/spark/spark_session.py
+++
@@ -1,9 +0,0 @@
-from pyspark.sql import SparkSession
-
-
-def get_spark_session(app_name: str = "Spark App") -> SparkSession:
-    return (SparkSession.builder
-            # .enableHiveSupport()
-            .appName(app_name)
-            # .config('spark.useHiveContext', 'true')
-            .getOrCreate())
---
+++ lighthouse/spark/spark_sessions.py
@@ -0,0 +1,46 @@
+from abc import ABC, abstractmethod
+from typing import Dict
+from pyspark.sql import SparkSession
+
+
+class SparkSessions(ABC):
+
+    @abstractmethod
+    def enable_hive_support(self) -> bool:
+        raise NotImplementedError
+
+    @abstractmethod
+    def spark_options(self) -> Dict[str, str]:
+        raise NotImplementedError
+
+    @staticmethod
+    def default_configuration() -> Dict[str, str]:
+        return {
+            "spark.driver.memory": "2g",
+            "spark.serializer": "org.apache.spark.serializer.KryoSerializer",
+            "spark.sql.avro.compression.codec": "snappy",
+            "spark.sql.parquet.compression.codec": "snappy",
+            "spark.sql.sources.partitionColumnTypeInference.enabled": "false",
+            "spark.sql.sources.partitionOverwriteMode": "dynamic"
+        }
+
+    def spark_session_builder(self) -> SparkSession.Builder:
+        builder = SparkSession.Builder()
+
+        joined_config: Dict[str, str] = (
+            self.spark_options()
+                .copy()
+                .update(self.default_configuration())
+                .self)
+        for key, value in joined_config.items():
+            builder.config(key, value)
+
+        return builder
+
+    def spark(self) -> SparkSession:
+        builder = self.spark_session_builder()
+
+        if self.enable_hive_support():
+            builder.enableHiveSupport()
+
+        return builder.getOrCreate()
